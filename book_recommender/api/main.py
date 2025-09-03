84b547cdaf6aa802c215f97ebc023591fd0dd98f
karthickb-runk <karthickb@runktechnology.com>
2025-09-03 15:15:40 +0530

    [BR]-feat: Adding render deploy files

--- book_recommender/api/main.py
+++ book_recommender/api/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from book_recommender.api import router
+
+origins = ["*"]
 
 app = FastAPI(title="Book Recommendation API")
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
